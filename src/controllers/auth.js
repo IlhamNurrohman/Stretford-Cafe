57112f5f845ab1a514a4845881d5f66479f16564
ilhamnurrohman <ilhamnurrohman74@gmail.com>
2022-06-15 22:31:23 +0700

    auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -55,7 +55,7 @@ auth.signIn = async (req, res) => {
     // return
     successResponse(res, 200, { email, token, auth: data.roles_id }, null);
   } catch (error) {
-    //console.log(error, payload);
+    //console.log(error);
     const { status = 500 ,err } = error;
     errorResponse(res, status, err);
   }
